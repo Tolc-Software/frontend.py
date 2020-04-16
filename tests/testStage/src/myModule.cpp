e74a60804342b6b1a839f47503990a40e996c1bb
srydell <simonwrydell@gmail.com>
2020-04-16 17:50:36 +0200

    Added some more examples and changed the module name so it can be used by python

--- tests/testStage/src/myModule.cpp
+++ tests/testStage/src/myModule.cpp
@@ -1,3 +1,4 @@
+#include "test.hpp"
 #include <pybind11/pybind11.h>
 
 namespace py = pybind11;
@@ -6,7 +7,18 @@ int add(int i, int j) {
 	return i + j;
 }
 
-PYBIND11_MODULE(example, m) {
-	m.doc() = "pybind11 example plugin";// optional module docstring
+PYBIND11_MODULE(myModule, m) {
+	// NOTE:    ^-- This name needs to be the same as the CMake target
+
+	// optional module docstring
+	m.doc() = "pybind11 example plugin";
+
+	// Adding a simple function with optional help text
 	m.def("add", &add, "A function which adds two numbers");
+
+	// Adding a submodule within this module
+	auto myNamespace = m.def_submodule("MyNamespace");
+	// Adding functions within a namespace
+	// is done with a fully qualified name
+	myNamespace.def("sayHello", &MyNamespace::sayHello);
 }
