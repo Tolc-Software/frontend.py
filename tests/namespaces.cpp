087eda82e14d04d5a3cb1719728d36e6cb629e0f
srydell <simonwrydell@gmail.com>
2020-04-21 20:49:22 +0200

    Refactored test cmakelists.txt and added function with return value to the tests

--- tests/namespaces.cpp
+++ tests/namespaces.cpp
@@ -5,43 +5,24 @@
 #include <catch2/catch.hpp>
 #include <fmt/format.h>
 
-TEST_CASE("Write to file functions", "[functions]") {
+TEST_CASE("Namespaces turn into modules", "[namespaces]") {
 	std::string moduleName = "defaultModule";
 	auto stage =
 	    TestUtil::PybindStage(TestStage::getRootStagePath(), moduleName);
+	stage.keepAliveAfterTest();
 
 	auto cppCode = R"(
-#include <fstream>
-#include <string>
+namespace MyLib {
 
-void sayHello() {
-	std::ofstream f("hello.txt");
-	f << "Hello!";
-	f.close();
-}
-
-void addYourOwn(std::string content) {
-	std::ofstream f("hello.txt");
-	f << content;
-	f.close();
-}
-
-int calculate() {
+int complexFunction() {
 	return 5;
 }
+}
+
 )";
 
 	auto pythonTestCode = fmt::format(R"(
-{moduleName}.sayHello()
-with open("hello.txt", "r") as f:
-    self.assertEqual(f.readline(), "Hello!")
-
-content = "This is from python!"
-{moduleName}.addYourOwn(content)
-with open("hello.txt", "r") as f:
-    self.assertEqual(f.readline(), content)
-
-result = {moduleName}.calculate()
+result = {moduleName}.MyLib.complexFunction()
 self.assertEqual(result, 5)
 )",
 	                                  fmt::arg("moduleName", moduleName));
@@ -49,4 +30,3 @@ self.assertEqual(result, 5)
 	auto errorCode = TestUtil::runPybindTest(stage, cppCode, pythonTestCode);
 	REQUIRE(errorCode == 0);
 }
-
