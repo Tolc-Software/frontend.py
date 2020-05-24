3952af4f34830268fb5be8a58c7bde9995af3826
srydell <simonwrydell@gmail.com>
2020-05-24 11:19:14 +0200

    Added enum example

--- tests/testStage/src/myModule.cpp
+++ tests/testStage/src/myModule.cpp
@@ -15,6 +15,10 @@ std::vector<int> getValues(int i, int j) {
 	return {i, j};
 }
 
+enum class WorldlyThings { Money, Status, MeaningOfLife };
+
+enum ThingsIWant { BetterChair, Abs, WarmSocks };
+
 PYBIND11_MODULE(myModule, myModule) {
 	// NOTE:    ^-- This name needs to be the same as the CMake target
 
@@ -34,6 +38,23 @@ PYBIND11_MODULE(myModule, myModule) {
 	             "Get the input as an array",
 	             py::arg("i"),
 	             py::arg("j"));
+
+	// NOTE: Scoped enums have 'py::arithmetic()'
+	//       This adds additional convenience functions such as
+	//       comparisons, and, or etc.
+	py::enum_<WorldlyThings>(myModule, "WorldlyThings", py::arithmetic())
+	    .value("Money", WorldlyThings::Money)
+	    .value("Status", WorldlyThings::Status)
+	    .value("MeaningOfLife", WorldlyThings::MeaningOfLife);
+
+	// NOTE: Unscoped enums have 'export_values()'
+	//       This exports them to the parent namespace (here myModule)
+	//       This is more closely to how they are used in C++
+	py::enum_<ThingsIWant>(myModule, "ThingsIWant")
+	    .value("BetterChair", ThingsIWant::BetterChair)
+	    .value("Abs", ThingsIWant::Abs)
+	    .value("WarmSocks", ThingsIWant::WarmSocks)
+	    .export_values();
 
 	// Adding a submodule within this module
 	auto myNamespace = myModule.def_submodule("MyNamespace");
