3e3a274b2ce439ea59761b7e23a9141e8f9d6cdd
srydell <simonwrydell@gmail.com>
2021-01-30 16:58:09 +0100

    Add a test for namespace with the same name as the library

---
+++ tests/namespaceSameAsModule.cpp
@@ -0,0 +1,31 @@
+#include "Frontend/Python/frontend.hpp"
+#include "TestStage/paths.hpp"
+#include "TestUtil/pybindStage.hpp"
+#include "TestUtil/runPybindTest.hpp"
+#include <catch2/catch.hpp>
+#include <fmt/format.h>
+
+TEST_CASE("Namespace name same as module", "[namespaceSameAsModule]") {
+	std::string moduleName = "defaultModule";
+	auto stage =
+	    TestUtil::PybindStage(TestStage::getRootStagePath(), moduleName);
+
+	auto cppCode = R"(
+namespace defaultModule {
+
+int complexFunction() {
+	return 5;
+}
+
+})";
+
+	auto pythonTestCode = fmt::format(R"(
+result = {moduleName}.{moduleName}.complexFunction()
+self.assertEqual(result, 5)
+)",
+	                                  fmt::arg("moduleName", moduleName));
+
+	auto errorCode =
+	    TestUtil::runPybindTest(stage, cppCode, pythonTestCode, moduleName);
+	REQUIRE(errorCode == 0);
+}
