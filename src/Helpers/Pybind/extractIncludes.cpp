2ae9697b3c8a8d75381648d3f67218f79f234f81
Simon Rydell <simonwrydell@gmail.com>
2021-04-22 22:04:46 +0200

    Support deques and lists. Give error on unsupported stdlib containers

--- src/Helpers/Pybind/extractIncludes.cpp
+++ src/Helpers/Pybind/extractIncludes.cpp
@@ -2,6 +2,7 @@
 #include <IR/ir.hpp>
 #include <queue>
 #include <set>
+#include <spdlog/spdlog.h>
 #include <string>
 
 namespace {
@@ -29,22 +30,38 @@ std::set<std::string> extractIncludes(IR::Type const& type) {
 	while (!containersToCheck.empty()) {
 		IR::Type::Container const& current = containersToCheck.front();
 
+		// See https://pybind11.readthedocs.io/en/stable/advanced/cast/overview.html
 		switch (current.m_container) {
 			case IR::ContainerType::Array:
-			case IR::ContainerType::Map:
-			case IR::ContainerType::Set:
-			case IR::ContainerType::UnorderedMap:
-			case IR::ContainerType::UnorderedSet:
 			case IR::ContainerType::Vector:
+			case IR::ContainerType::Deque:
+			case IR::ContainerType::List:
+			case IR::ContainerType::Map:
+			case IR::ContainerType::UnorderedMap:
+			case IR::ContainerType::Set:
+			case IR::ContainerType::UnorderedSet:
 				includes.insert("<pybind11/stl.h>");
 				break;
+			case IR::ContainerType::ForwardList:
+			case IR::ContainerType::MultiMap:
+			case IR::ContainerType::MultiSet:
+			case IR::ContainerType::PriorityQueue:
+			case IR::ContainerType::Queue:
+			case IR::ContainerType::Stack:
+			case IR::ContainerType::UnorderedMultiMap:
+			case IR::ContainerType::UnorderedMultiSet:
+				spdlog::error(
+				    "Container type {} does not currently have a direct translation via pybind11. The translation might not work.",
+				    type.m_representation);
+				break;
+
 			case IR::ContainerType::Allocator:
+			case IR::ContainerType::EqualTo:
 			case IR::ContainerType::Greater:
 			case IR::ContainerType::Hash:
 			case IR::ContainerType::Less:
 			case IR::ContainerType::Pair:
-			case IR::ContainerType::Tuple:
-			case IR::ContainerType::EqualTo: break;
+			case IR::ContainerType::Tuple: break;
 		}
 
 		for (auto const& containedType : current.m_containedTypes) {
